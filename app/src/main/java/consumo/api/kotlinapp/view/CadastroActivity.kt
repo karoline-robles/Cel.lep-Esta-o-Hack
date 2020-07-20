994f50d6e8cb59cc09f2b1e2e59067eb4558a844
karoline-robles <karolinerobles.kr@gmail.com>
2020-07-20 20:43:11 -0300

    Validação de campos em Kotlin

--- app/src/main/java/consumo/api/kotlinapp/view/CadastroActivity.kt
+++ app/src/main/java/consumo/api/kotlinapp/view/CadastroActivity.kt
@@ -1,12 +1,40 @@
 package consumo.api.kotlinapp.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import consumo.api.kotlinapp.HomeActivity
 import consumo.api.kotlinapp.R
+import kotlinx.android.synthetic.main.activity_cadastro.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+        buttonCadastr.setOnClickListener{
+            validarCampos()
+        }
+
+
+    }
+    private fun validarCampos (){
+        val nome = input_nome.text.toString().trim()
+        val telefone = input_telefone.text.toString().trim()
+        val email = input_email.text.toString().trim()
+        val senha = input_senha.text.toString().trim()
+
+        if(nome.isEmpty() && telefone.isEmpty() && email.isEmpty() && senha.isEmpty()) {
+            input_nome.error = "Preencha os campos!"
+            input_telefone.error = "Preencha os campos!"
+            input_email.error = "Preencha os campos!"
+            input_senha.error = "Preencha os campos!"
+            false
+        }else{
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+            true
+        }
+
     }
 }
--- app/src/main/java/consumo/api/kotlinapp/view/LoginActivity.kt
+++ app/src/main/java/consumo/api/kotlinapp/view/LoginActivity.kt
@@ -1,12 +1,46 @@
 package consumo.api.kotlinapp.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import consumo.api.kotlinapp.HomeActivity
 import consumo.api.kotlinapp.R
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+            validaCampos()
+            trocaTela()
+    }
+
+    private fun validaCampos() {
+        buttonEntrar.setOnClickListener {
+
+            val email = email_input.text.toString()
+            val senha = senha_input.text.toString()
+
+            if (email.isEmpty() && senha.isEmpty()) {
+                email_input.error = "Preencha os campos!"
+                senha_input.error = "Preencha os campos!"
+                false
+            } else {
+                startActivity(Intent(this, HomeActivity::class.java))
+                finish()
+                true
+            }
+        }
+    }
+    private fun trocaTela(){
+        buttonCadastro.setOnClickListener() {
+            startActivity(Intent(this, CadastroActivity::class.java))
+            finish()
+        }
+    }
+    private fun testeSpinner(){
+        var generos = arrayOf("Feminino", "Masculino", "Não binário", "Outros")
     }
 }
